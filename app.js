4ca25c1bc48d0ab767bdec8ede548818a4d2514c
Yetunde Sola-Adebayo <yetundesola-adebayo@Yetundes-Air.attlocal.net>
2019-06-20 19:49:09 -0500

    added google captcha

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ var express = require("express"),
   expressSanitizer = require("express-sanitizer"), //remove js script from code
   methodeOverride = require("method-override"),
   bodyParser = require("body-parser"),
+  request = require("request"),
   passport = require("passport"),
   LocalStrategy = require("passport-local"),
-  passportLocalMongoose = require("passport-local-mongoose"),
   User = require("./models/user"),
   Blog = require("./models/blog"),
-  seedDB = require("./seeds.js"),
   Comment = require("./models/comment"),
   nodemailer = require("nodemailer"),
+  alert = require("alert-node"),
   mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config({ path: __dirname + "/.env" });
 
 var url = process.env.DATABASEURL || "mongodb://localhost/restful_blog_app";
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5080;
 // seedDB();
 mongoose
-  .connect(url)
+  .connect(url, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
+mongoose.set("useFindAndModify", false);
 
 app.set("view engine", "ejs");
 
@@ -46,14 +47,14 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   next();
 });
 
 // INDEX ROUTE
-app.get("/", function (req, res) {
-  Blog.find({}, function (err, blogs) {
+app.get("/", function(req, res) {
+  Blog.find({}, function(err, blogs) {
     if (err) {
       console.log("ERROR!");
     } else {
@@ -63,22 +64,22 @@ app.get("/", function (req, res) {
 });
 
 //NEW ROUTE
-app.get("/new", isLoggedIn, function (req, res) {
+app.get("/new", isLoggedIn, function(req, res) {
   res.render("blogs/new");
 });
 
 //ABOUT ROUTE
-app.get("/about", function (req, res) {
+app.get("/about", function(req, res) {
   res.render("blogs/about");
 });
 
 //CONTACT ROUTE
-app.get("/contact", function (req, res) {
+app.get("/contact", function(req, res) {
   res.render("blogs/contact");
 });
 
 // POST route from contact form
-app.post("/contact", function (req, res) {
+app.post("/contact", function(req, res) {
   let mailOpts, smtpTrans;
   smtpTrans = nodemailer.createTransport({
     host: process.env.NODEMAILER_HOST,
@@ -95,9 +96,9 @@ app.post("/contact", function (req, res) {
     subject: "New message from contact form at yettifood",
     text: `${req.body.name} ${req.body.subject} (${req.body.email}) says: ${
       req.body.message
-      }`
+    }`
   };
-  smtpTrans.sendMail(mailOpts, function (error, response) {
+  smtpTrans.sendMail(mailOpts, function(error, response) {
     if (error) {
       console.log(error);
     } else {
@@ -107,9 +108,9 @@ app.post("/contact", function (req, res) {
 });
 
 // CREATE ROUTE
-app.post("/blogs", function (req, res) {
+app.post("/blogs", function(req, res) {
   // create blog
-  Blog.create(req.body.blog, function (err, newBlog) {
+  Blog.create(req.body.blog, function(err, newBlog) {
     //req.body everything 4rm form. req.body.blog: it contains everything in blog array,  e.g blog[title], so it creates blog with that info
     if (err) {
       res.render("blogs/new");
@@ -121,10 +122,10 @@ app.post("/blogs", function (req, res) {
 });
 
 //SHOW ROUTE
-app.get("/blogs/:id", function (req, res) {
+app.get("/blogs/:id", function(req, res) {
   Blog.findById(req.params.id)
     .populate("comments")
-    .exec(function (err, foundBlog) {
+    .exec(function(err, foundBlog) {
       if (err) {
         res.redirect("/");
       } else {
@@ -135,8 +136,8 @@ app.get("/blogs/:id", function (req, res) {
 });
 
 //EDIT ROUTE
-app.get("/blogs/:id/edit", function (req, res) {
-  Blog.findById(req.params.id, function (err, foundBlog) {
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
     if (err) {
       res.redirect("/");
     } else {
@@ -146,8 +147,8 @@ app.get("/blogs/:id/edit", function (req, res) {
 });
 
 //UPDATE ROUTE
-app.put("/blogs/:id", function (req, res) {
-  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (
+app.put("/blogs/:id", function(req, res) {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
     err,
     UpdatedBlog
   ) {
@@ -161,8 +162,8 @@ app.put("/blogs/:id", function (req, res) {
 });
 
 //DELETE ROUTE
-app.delete("/blogs/:id", function (req, res) {
-  Blog.findByIdAndRemove(req.params.id, function (err) {
+app.delete("/blogs/:id", function(req, res) {
+  Blog.findByIdAndRemove(req.params.id, function(err) {
     //destroy blog
     if (err) {
       res.redirect("/"); //redirect
@@ -176,9 +177,9 @@ app.delete("/blogs/:id", function (req, res) {
 ///COMMENTS ROUTE
 ////////////
 
-app.get("/blogs/:id/comments/new", function (req, res) {
+app.get("/blogs/:id/comments/new", function(req, res) {
   //find camo by id
-  Blog.findById(req.params.id, function (err, blog) {
+  Blog.findById(req.params.id, function(err, blog) {
     if (err) {
       console.log(err);
     } else {
@@ -187,28 +188,53 @@ app.get("/blogs/:id/comments/new", function (req, res) {
   });
 });
 
-app.post("/blogs/:id/comments", function (req, res) {
-  //lookup blog with id, create new comment, connect comment to blog, redirect to blog showpage
-  Blog.findById(req.params.id, function (err, blog) {
-    if (err) {
-      console.log(err);
-    } else {
-      Comment.create(req.body.comment, function (err, comment) {
-        if (err) {
-          console.log(err);
-        } else {
-          blog.comments.push(comment);
-          blog.save();
-          res.redirect("/blogs/" + blog._id);
-        }
-      });
-    }
-  });
+app.post("/blogs/:id/comments", function(req, res) {
+  var secretKey = process.env.G_CAPTCHA;
+  // req.connection.remoteddress will provide IP address of connected user.
+  var verificationUrl =
+    "https://www.google.com/recaptcha/api/siteverify?secret=" +
+    secretKey +
+    "&response=" +
+    req.body["g-recaptcha-response"] +
+    "&remoteip=" +
+    req.connection.remoteAddress;
+
+  if (
+    req.body["g-recaptcha-response"] === undefined ||
+    req.body["g-recaptcha-response"] === "" ||
+    req.body["g-recaptcha-response"] === null
+  ) {
+    alert("Please select captcha");
+  } else {
+    request(verificationUrl, function(error, response, body) {
+      body = JSON.parse(body);
+      if (body.success !== undefined && !body.success) {
+        alert("Failed captcha verification");
+      } else {
+        //lookup blog with id, create new comment, connect comment to blog, redirect to blog showpage
+        Blog.findById(req.params.id, function(err, blog) {
+          if (err) {
+            console.log(err);
+          } else {
+            Comment.create(req.body.comment, function(err, comment) {
+              if (err) {
+                console.log(err);
+              } else {
+                blog.comments.push(comment);
+                blog.save();
+                res.redirect("/blogs/" + blog._id);
+              }
+            });
+          }
+        });
+      }
+    });
+  } //end else
 });
 
 // COMMENT EDIT ROUTE
-app.get("/comment/:comment_id/edit", function (req, res) {
-  Comment.findById(req.params.comment_id, function (err, foundComment) {
+app.get("/comment/:comment_id/edit", function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
     if (err) {
       res.redirect("/");
     } else {
@@ -218,8 +244,8 @@ app.get("/comment/:comment_id/edit", function (req, res) {
 });
 
 // COMMENT UPDATE
-app.put("/comment/:comment_id", function (req, res) {
-  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (
+app.put("/comment/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(
     err,
     updatedComment
   ) {
@@ -232,9 +258,9 @@ app.put("/comment/:comment_id", function (req, res) {
 });
 
 // COMMENT DESTROY ROUTE
-app.delete("/blogs/:id/comment/:comment_id", function (req, res) {
+app.delete("/blogs/:id/comment/:comment_id", function(req, res) {
   //findByIdAndRemove
-  Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
     if (err) {
       res.redirect("/");
     } else {
@@ -246,22 +272,22 @@ app.delete("/blogs/:id/comment/:comment_id", function (req, res) {
 //comment end
 
 //register
-app.get("/register", function (req, res) {
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function (req, res) {
+app.post("/register", function(req, res) {
   req.body.username;
   req.body.password;
   User.register(
     new User({ username: req.body.username }),
     req.body.password,
-    function (err, user) {
+    function(err, user) {
       if (err) {
         console.log(err);
         return res.render("register"); //if error go back to register
       }
-      passport.authenticate("local")(req, res, function () {
+      passport.authenticate("local")(req, res, function() {
         //local strategy, can be changed to another eg twitter
         res.redirect("/new"); //once registered you go to secret page
       });
@@ -270,7 +296,7 @@ app.post("/register", function (req, res) {
 });
 
 //Login Route
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
@@ -283,11 +309,11 @@ app.post(
     successRedirect: "/", //if succesfull
     failureRedirect: "/login" //if failed
   }),
-  function (req, res) { }
+  function(req, res) {}
 );
 
 //Logout Route
-app.get("/logout", function (req, res) {
+app.get("/logout", function(req, res) {
   req.logout(); //logout is a fun from passport
   res.redirect("/"); //redirects to homepage
 });
@@ -301,6 +327,6 @@ function isLoggedIn(req, res, next) {
 }
 //end of reg
 
-app.listen(port, process.env.IP, function () {
+app.listen(port, process.env.IP, function() {
   console.log("server started");
 });
